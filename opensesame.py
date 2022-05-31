a74f8990c0f2726840586e5b8d56a89baedc7d6a
Joseph Hobbs <josephrosshobbs@gmail.com>
2022-05-31 13:47:20 -0400

    Initial commit

---
+++ opensesame.py
@@ -0,0 +1,7 @@
+# Code for actually opening and closing the garage door
+
+def open_door():
+    print("Opening the garage door")
+
+def close_door():
+    print("Closing the garage door")
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for, request, redirect
+from markupsafe import escape
+import opensesame
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/open", methods=["GET", "POST"])
+def open_door():
+    if request.method == "POST":
+        # Open the garage door
+        opensesame.open_door()
+    return redirect("/")
+
+@app.route("/close", methods=["GET", "POST"])
+def close_door():
+    if request.method == "POST":
+        # Close the garage door
+        opensesame.close_door()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="192.168.1.68", port=5000, debug=True)
